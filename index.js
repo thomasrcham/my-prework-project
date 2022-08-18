a69ab8bf8bfb197a65b68c85ed13047805a1f91a
thomasrcham <guseldorph@gmail.com>
2022-08-18 10:49:21 -0500

    made bunny show up

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+let bunnySpace;
 
-document.querySelector("body").appendChild(h2);
+function bunnySpace1 (src, width, height, alt){
+    currentScene = 1;
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img)
+}
+
+bunnySpace1 ("./images/bunny-1.png", 100, 100, "bunny");
