09e3616cb18e5228cc2acc67db8432f2cde31446
thomasrcham <guseldorph@gmail.com>
2022-08-18 13:41:59 -0500

    second bunny

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-/*let bunnySpace1;*/
-
-/*function bunnySpace1 (src, alt){
-    currentScene = 1;
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = 100;
-    img.height = 200;
-    img.alt = alt;
-    img.className = "bunny-1";
-    document.body.appendChild(img)
-}*/
-
-const bunnyMove1 = document.getElementById("bunny-1");
 
 function bunnyMove() {
-        document.getElementById("bunny-1").style.left = "200px";
+    document.getElementById("bunny-1").onmouseover = bunnyMove2();
+    document.getElementById("bunny-1").style.visibility = "hidden";
+    document.getElementById("bunny-2").style.visibility = "visible";
+    document.getElementById("heading").innerText = "Well that was rude."
+}          
+
+function bunnyMove2() {
+    document.getElementById("heading").innerText = "check"
 }
 
-/*bunnySpace1 ("./images/bunny-1.png", "bunny");*/
